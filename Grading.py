9e98d85e594186e3e380280885adb1ea174538b0
Hephzibah <94688043+hephzi06@users.noreply.github.com>
2022-06-02 12:42:12 +0100

    made changes in grammar

--- Grading.py
+++ Grading.py
@@ -4,14 +4,14 @@ if 75 <= grade <= 100:
 elif 60 <= grade:
     print(f'Very Good your score is {grade}')
 elif 45 <= grade:
-    print(f'Bad score but you can rewrite the exam do you want to know your score ')
+    print(f'Bad score but you can rewrite the exam. Do you want to know your score? ')
     pick = input("(Y)es or (N)o: ").lower()
     if pick == "y":
         print(f'Your score is {grade}')
     elif pick == "n":
-        print(f"Don't forget to retake the exam")
+        print(f"Don't forget to retake the exam. ")
 else:
-    print(f'You failed')
+    print(f'You failed. Do you want to know your score?')
     pick = input("(Y)es or (N)o: ").lower()
     if pick == "y":
         print(f'Your score is {grade}')
