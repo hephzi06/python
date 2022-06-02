b10d78ce9921bc01cf37ec9de97510444fc22feb
Hephzibah <94688043+hephzi06@users.noreply.github.com>
2022-06-02 11:14:36 +0100

    finshed it

---
+++ Grading.py
@@ -0,0 +1,19 @@
+grade = int(input("Enter your grade score: "))
+if 75 <= grade <= 100:
+    print(f"Excellent your score is {grade}")
+elif 60 <= grade:
+    print(f'Very Good your score is {grade}')
+elif 45 <= grade:
+    print(f'Bad score but you can rewrite the exam do you want to know your score ')
+    pick = input("(Y)es or (N)o: ").lower()
+    if pick == "y":
+        print(f'Your score is {grade}')
+    elif pick == "n":
+        print(f"Don't forget to retake the exam")
+else:
+    print(f'You failed')
+    pick = input("(Y)es or (N)o: ").lower()
+    if pick == "y":
+        print(f'Your score is {grade}')
+    elif pick == "n":
+        print(f"Don't forget to retake the exam")
