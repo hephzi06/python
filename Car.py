96fe7b0edb4aba8c3e197882b6e8350229abb4c0
Hephzibah <94688043+hephzi06@users.noreply.github.com>
2022-06-03 09:56:47 +0100

    added some stuff to car.py

--- Car.py
+++ Car.py
@@ -17,9 +17,41 @@
 # Mosh correction
 """correction by mosh"""
 
+# command = ""
+# started = False
+# stopped = False
+# while True:
+#     command = input("> ").lower()
+#     if command == "start":
+#
+#         if started:
+#             print("Car is already started")
+#         else:
+#             started = True
+#             stopped = False
+#             print("Car started ...")
+#     elif command == "stop":
+#         if stopped:
+#             print("Car already stopped")
+#         else:
+#             started = False
+#             stopped = True
+#             print("Car stopped .. ")
+#     elif command == "help":
+#         print("""
+# start - to start the car
+# stop - to stop the car
+# quit - to quit
+#         """)
+#     elif command == "quit":
+#         break
+#     else:
+#         print("Sorry I don't understand that")
+
+
 command = ""
 started = False
-stopped = False
+
 while True:
     command = input("> ").lower()
     if command == "start":
@@ -28,21 +60,20 @@ while True:
             print("Car is already started")
         else:
             started = True
-            stopped = False
+
             print("Car started ...")
     elif command == "stop":
-        if stopped:
+        if not started:
             print("Car already stopped")
         else:
             started = False
-            stopped = True
             print("Car stopped .. ")
     elif command == "help":
         print("""
-start - to start the car
-stop - to stop the car
-quit - to quit
-        """)
+ start - to start the car
+ stop - to stop the car
+ quit - to quit
+         """)
     elif command == "quit":
         break
     else:
