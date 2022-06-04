e766386b3ca0358f4cf3f22d073b64ac40e07fd5
Hephzibah <94688043+hephzi06@users.noreply.github.com>
2022-06-04 14:04:42 +0100

    addded function

---
+++ Function.py
@@ -0,0 +1,9 @@
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}!")
+    print("Welcome aboard")
+
+
+print("Start")
+greet_user(last_name="Mary", first_name="John")
+
+print("Finish")
