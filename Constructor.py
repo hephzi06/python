8d5df5ebb884eaf3cd65898d6a59b2891f191136
Hephzibah <94688043+hephzi06@users.noreply.github.com>
2022-06-05 10:54:33 +0100

    added some new files

---
+++ Constructor.py
@@ -0,0 +1,14 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("Draw")
+
+
+point = Point(10, 10)
+print(point.x)
