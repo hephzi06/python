55a7bcf7d2ea4d643a6c1d04590ebe924b7c0478
Hephzibah <94688043+hephzi06@users.noreply.github.com>
2022-06-14 17:17:51 +0100

    added transaction

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
-from Ecommerce import shipping
-
-shipping.calc_shipping()
+# from Ecommerce import shipping
+#
+# shipping.calc_shipping()
+from pathlib import Path
+path = Path()
+for file in path.glob('*'):
+    print(file)
